5426786ea3f9acef4ceb6a32211a108974340ba9
Schwaips <charlie.bertrand@live.com>
2023-10-08 10:30:37 +0200

    adding lead model

---
+++ db/migrate/20231008082948_create_leads.rb
@@ -0,0 +1,17 @@
+class CreateLeads < ActiveRecord::Migration[7.0]
+  def change
+    create_table :leads do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :company
+      t.string :type
+      t.text :message
+      t.string :phone_number
+      t.boolean :newsletter
+      t.boolean :terms
+
+      t.timestamps
+    end
+  end
+end
