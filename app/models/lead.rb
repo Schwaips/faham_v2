0b8ed46ab3bb124899f522cd2d22171a5730ff3f
Schwaips <charlie.bertrand@live.com>
2024-01-30 08:07:36 +0100

    Faham active admin setup

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -6,4 +6,8 @@ class Lead < ApplicationRecord
   validates :email, :format => { :with => %r{.+@.+\..+} }, allow_blank: true
 
   validates :customer_type, presence: true, inclusion: { in: TYPES }
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["company", "created_at", "customer_type", "email", "first_name", "id", "last_name", "message", "newsletter", "phone_number", "terms", "updated_at"]
+  end
 end
