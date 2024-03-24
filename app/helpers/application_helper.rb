fda5366c6d5f2f335e3db3d63a708c0efc5caf6c
Schwaips <charlie.bertrand@live.com>
2024-03-24 11:56:43 +0100

    [FEATURE] Adding style for menu and corecting link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def remove_navbar?(fullpath)
     false
   end
+
+  def is_root_path?
+    request.fullpath == '/'
+  end
+
+  def is_blog_path?
+    request.fullpath.start_with?('/articles')
+  end
+
+  def is_admin_path?
+    request.fullpath.start_with?('/admin')
+  end
 end
